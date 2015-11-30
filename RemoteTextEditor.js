1127c6d5a1962e0a36dad3a40e8fec3efed2f0c0
Jorge <me.mark.two@gmail.com>
2015-11-30 13:33:49 -0500

    feat: basic html page setup

---
+++ RemoteTextEditor.js
@@ -0,0 +1,32 @@
+function RemoteTextEditor(textBoxID, textFileSelectID, filesDir) {
+	this.textBoxID = textBoxID;
+	this.textFileSelectID = textFileSelectID;
+	this.filesDir = filesDir;
+
+	this.SaveFile = (function(fileName) {
+
+	}).bind(this)
+
+
+
+
+	this.GetFileList = (function() {
+		// Create request
+
+		// Create request function
+	}).bind(this)
+
+
+
+
+	this.LoadFileContents = (function() {
+
+	}).bind(this)
+
+
+
+
+	this.Sample = (function() {
+
+	}).bind(this)
+}
