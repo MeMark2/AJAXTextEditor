481c2a924d4e77f10da892adaa2f9b7a4d5a9298
Jorge <me.mark.two@gmail.com>
2015-11-30 14:59:26 -0500

    feat: build GetFileList method

--- RemoteTextEditor.js
+++ RemoteTextEditor.js
@@ -2,8 +2,13 @@ function RemoteTextEditor(textBoxID, textFileSelectID, filesDir) {
 	this.textBoxID = textBoxID;
 	this.textFileSelectID = textFileSelectID;
 	this.filesDir = filesDir;
+	this.xmlhttp = new XMLHttpRequest();
 
 	this.SaveFile = (function(fileName) {
+
+		xmlhttp.onreadystatechange = (function() {
+
+		}).bind(this)
 
 	}).bind(this)
 
@@ -11,9 +16,27 @@ function RemoteTextEditor(textBoxID, textFileSelectID, filesDir) {
 
 
 	this.GetFileList = (function() {
-		// Create request
-
 		// Create request function
+		xmlhttp.onreadystatechange = function(selectEleID){
+			var filesArray = JSON.parse(xmlhttp.responseText);
+			var selectEle = document.getElementById(selectEleID);
+			var selectOptions = "";
+
+			while (selectEle.length > 0) {
+				selectEle.remove(0);
+			}
+
+			for (var file = 0; file < filesArray.length; file++) {
+				selectEle.add(filesArray[file]);
+			};
+
+		}(this.textFileSelectID);
+
+		// Create request
+		xmlhttp.open("GET", "GetFileList.php");
+
+		// send request
+		xmlhttp.send(null);
 	}).bind(this)
 
 
